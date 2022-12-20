0541c6a7dcf4f1d8dd41739b702e5d606603a016
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2022-12-20 10:15:46 -0300

    import challenge files

---
+++ three-column-preview-card/src/Components/GlobalStyles.ts
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    font-family: "Montserrat", sans-serif;
+    font-size: 15px;
+    margin: 0;
+    padding: 0;
+    text-decoration: none;
+  }
+`;
--- three-column-preview-card/src/index.tsx
+++ three-column-preview-card/src/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { GlobalStyles } from './Components/GlobalStyles';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-  </React.StrictMode>
+    <GlobalStyles />
+  </React.StrictMode>,
 );
